87f8e15d353d7de73e5f5a982c8e1aa83919489f
Fernando Cardenas <fernandocardenasm@Fernandos-MacBook-Pro.local>
2015-11-29 18:19:36 +0100

    Add Sign up and Log in

---
+++ ParseStarterProject/ViewController.swift
@@ -0,0 +1,103 @@
+/**
+* Copyright (c) 2015-present, Parse, LLC.
+* All rights reserved.
+*
+* This source code is licensed under the BSD-style license found in the
+* LICENSE file in the root directory of this source tree. An additional grant
+* of patent rights can be found in the PATENTS file in the same directory.
+*/
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController{
+    
+    var activityIndicator:UIActivityIndicatorView = UIActivityIndicatorView()
+    
+    
+    @IBOutlet var username: UITextField!
+    @IBOutlet var password: UITextField!
+    
+    
+    
+    func displayAlert(title:String, message:String){
+        if #available(iOS 8.0, *) {
+            let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+            
+            alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: { (alert) -> Void in
+                self.dismissViewControllerAnimated(true, completion: nil)
+            }))
+            
+            self.presentViewController(alert, animated: true, completion: nil)
+            
+        }
+    }
+    
+    @IBAction func signUp(sender: AnyObject) {
+        if username.text == "" && password.text == "" {
+            displayAlert("Error in Form",message: "Please enter a username and password")
+        }
+        else {
+            activityIndicator.startAnimating()
+            UIApplication.sharedApplication().beginIgnoringInteractionEvents()
+            
+            let user = PFUser()
+            user.username = username.text
+            user.password = password.text
+            
+            user.signUpInBackgroundWithBlock {
+                (success: Bool, error: NSError?) -> Void in
+                if let error = error {
+                    if let errorMessage = error.userInfo["error"] as? NSString {
+                        self.displayAlert("Failed Sign up", message: errorMessage as String)
+                    }
+                    // Show the errorString somewhere and let the user try again.
+                } else {
+                    // Hooray! Let them use the app now.
+                    
+                }
+                self.activityIndicator.stopAnimating()
+                UIApplication.sharedApplication().endIgnoringInteractionEvents()
+            }
+        }
+    }
+    
+    @IBAction func logIn(sender: AnyObject) {
+        
+        activityIndicator.startAnimating()
+        UIApplication.sharedApplication().beginIgnoringInteractionEvents()
+        
+        PFUser.logInWithUsernameInBackground(username.text!, password:password.text!) {
+            (user: PFUser?, error: NSError?) -> Void in
+            if user != nil {
+                // Do stuff after successful login.
+            } else {
+                // The login failed. Check error to see why.
+                
+                if let errorMessage = error!.userInfo["error"] as? NSString {
+                    self.displayAlert("Failed Sign up", message: (errorMessage as String).capitalizedString)
+                }
+            }
+            self.activityIndicator.stopAnimating()
+            UIApplication.sharedApplication().endIgnoringInteractionEvents()
+        }
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        activityIndicator = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray
+        view.addSubview(activityIndicator)
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
